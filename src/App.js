ddf312fadb37ce6b1d75f04667c1c606fb6a47ca
Yash <yashgarg.co@gmail.com>
2023-07-16 20:28:48 +0530

    start

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Login from "./components/Login";
+import Header from "./components/Header";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Router>
+      <Header />
+      <Routes>
+      <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
